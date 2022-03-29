541a3cbb661328b38f550095892a3c63d7f518f3
Magistra <magistraagis@gmail.com>
2022-03-29 14:48:27 +0800

    super

--- codingBebas.cpp
+++ codingBebas.cpp
@@ -3,15 +3,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int d[] = {1, 3, 3, 4, 5, 2, 333, 5};
-    int n, f = 0, c;
-    for (int i = 0; i < n; i++)
-    {
-        if (c == d[i])
-        {
-            f++;
-        }
-    }
+   cout << "testing" << endl;
 
     return 0;
 }
